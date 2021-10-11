b5bc3f211b06cb2d1df7884c0710890bae07009a
Samantha Wittke <32324155+samumantha@users.noreply.github.com>
2021-10-11 17:41:16 +0300

    last years install list added

---
+++ test2_install_packages.sh
@@ -0,0 +1,22 @@
+install2.r --error --skipinstalled \
+    raster \
+    lubridate \
+    sp \
+    rgdal \
+    viridis \
+    mgcv \
+    snow \
+    sf \
+    tidyverse \
+    rgeos \
+    rmapshaper \
+    spdep \
+    spatstat \
+    RQGIS \
+    mapview \
+    mapedit \
+    fpc \
+    GWmodel \
+    NbClust \
+    reshape2 \
+    osmdata
